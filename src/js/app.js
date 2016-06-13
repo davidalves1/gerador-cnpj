4361c432b5cd57d08818d55c8c03327406c7819e
David Alves de Souza <alves.david@outlook.com>
2016-06-13 20:49:03 -0300

    Correção

--- src/js/app.js
+++ src/js/app.js
@@ -63,10 +63,6 @@ app.controller('MainController', ['$scope', function($scope) {
 
         // Exibe o cnpj no campo
         vm.cnpj = arr.join('');
-
-        if (vm.chkFormatarCnpj === true) {
-            vm.cnpj = vm.cnpj.replace(/(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/g,"\$1.\$2.\$3\/\$4\-\$5");
-        }
     }
 
     vm.formatarCnpj = formatarCnpj;
